a08c4353c55c834db1dd383e1995392c499587d2
gustavowittbecker <32279987+gustavowittbecker@users.noreply.github.com>
2023-03-08 17:06:29 -0300

    composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '23a14f9e64a21e962d33675f457dc2e30b5422cb',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '23a14f9e64a21e962d33675f457dc2e30b5422cb',
+    ),
+  ),
+);
