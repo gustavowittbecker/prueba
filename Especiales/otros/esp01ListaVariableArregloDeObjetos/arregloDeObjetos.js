fc07d7c5ed4ba8cd73c6114a7847faea776c6b7a
gustavowittbecker <32279987+gustavowittbecker@users.noreply.github.com>
2023-03-23 17:34:25 -0300

    Se agregan especiales

---
+++ Especiales/otros/esp01ListaVariableArregloDeObjetos/arregloDeObjetos.js
@@ -0,0 +1,14 @@
+﻿var jsonUsuarios = '{"usuarios": [' +
+'{"nombre":"Gustavo","añoNacimiento":"1956","saldoCuenta":1500},'+
+'{"nombre":"Ariel","añoNacimiento":"1957","saldoCuenta":794},'+
+'{"nombre":"Jose","añoNacimiento":"1958","saldoCuenta":1111},'+
+'{"nombre":"Miriam","añoNacimiento":"1959","saldoCuenta":1750},'+
+'{"nombre":"Roberto","añoNacimiento":"1960","saldoCuenta":2000},'+
+'{"nombre":"Pedro","añoNacimiento":"1961","saldoCuenta":2002},'+
+'{"nombre":"Francisco","añoNacimiento":"1962","saldoCuenta":2005},'+
+'{"nombre":"Omar","añoNacimiento":"1963","saldoCuenta":1999},'+
+'{"nombre":"Alberto","añoNacimiento":"1964","saldoCuenta":2000},'+
+'{"nombre":"Marta","añoNacimiento":"1965","saldoCuenta":2000},'+
+'{"nombre":"Carolina","añoNacimiento":"1966","saldoCuenta":2000},'+
+'{"nombre":"Pablo","añoNacimiento":"1967","saldoCuenta":2000}'+
+']}';
